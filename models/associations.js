87c6e94fcb5874e499209839436e44e0ac314ddf
Zaidoon Kamil <igdghfgjhfgvffv68@gmail.com>
2025-10-04 21:56:13 +0300

    .

--- models/associations.js
+++ models/associations.js
@@ -4,25 +4,13 @@ const Message = require("./message");
 const Settings = require("./settings");
 
 // تعريف العلاقات
-Room.belongsTo(User, { 
-    foreignKey: 'creatorId', 
-    as: 'creator' 
-});
+Room.belongsTo(User, { foreignKey: 'creatorId', as: 'creator', onDelete: 'CASCADE' });
 
-Room.hasMany(Message, { 
-    foreignKey: 'roomId', 
-    as: 'messages' 
-});
+Room.hasMany(Message, { foreignKey: 'roomId', as: 'messages', onDelete: 'CASCADE' });
 
-Message.belongsTo(User, { 
-    foreignKey: 'userId', 
-    as: 'user' 
-});
+Message.belongsTo(User, { foreignKey: 'userId', as: 'user', onDelete: 'CASCADE' });
 
-Message.belongsTo(Room, { 
-    foreignKey: 'roomId', 
-    as: 'room' 
-});
+Message.belongsTo(Room, { foreignKey: 'roomId', as: 'room', onDelete: 'CASCADE' });
 
 module.exports = {
     User,
