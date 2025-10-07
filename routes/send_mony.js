f82b79cb2a8ae7f9c1239ad8ed0a455e18a63597
Zaidoon Kamil <igdghfgjhfgvffv68@gmail.com>
2025-10-07 11:43:13 +0300

    .

--- routes/send_mony.js
+++ routes/send_mony.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const multer = require("multer");
-const upload = multer();
 const { User, DailyAction, UserCounter, Counter } = require("../models");
 const { Op } = require("sequelize");
 const TransferHistory = require("../models/transferHistory");
