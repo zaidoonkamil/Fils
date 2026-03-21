36d471540cc82b776166a4442da8b397cb350ef7
Zaidoon Kamil <igdghfgjhfgvffv68@gmail.com>
2026-03-21 15:36:28 +0300

    .

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ initializeSocketIO(roomNamespace);
 app.set("roomsIO", roomNamespace);
 
 
-const PORT = process.env.PORT || 1400;
+const PORT = process.env.PORT || 1300;
 const HOST = process.env.HOST || "0.0.0.0";
 
 server.listen(PORT, HOST, () => {
