803d20ef1158f35691d0c9c492e8f2bcda1d9f1e
Zaidoon Kamil <igdghfgjhfgvffv68@gmail.com>
2026-07-09 11:02:07 +0300

    .

--- models/DominoPrivateRoom.js
+++ models/DominoPrivateRoom.js
@@ -1,51 +1,57 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 
-const DominoPrivateRoom = sequelize.define("DominoPrivateRoom", {
-  code: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
+const DominoPrivateRoom = sequelize.define(
+  "DominoPrivateRoom",
+  {
+    code: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    hostUserId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    guestUserId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      defaultValue: null,
+    },
+    packageKey: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "classic_1",
+    },
+    entryFee: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    prize: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "waiting",
+    },
+    matchId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      defaultValue: null,
+    },
+    expiresAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: null,
+    },
   },
-  hostUserId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  guestUserId: {
-    type: DataTypes.INTEGER,
-    allowNull: true,
-    defaultValue: null,
-  },
-  packageKey: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    defaultValue: "classic_1",
-  },
-  entryFee: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    defaultValue: 0,
-  },
-  prize: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    defaultValue: 0,
-  },
-  status: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    defaultValue: "waiting",
-  },
-  matchId: {
-    type: DataTypes.INTEGER,
-    allowNull: true,
-    defaultValue: null,
-  },
-  expiresAt: {
-    type: DataTypes.DATE,
-    allowNull: true,
-    defaultValue: null,
-  },
-});
+  {
+    tableName: "domino_private_rooms",
+  }
+);
 
 module.exports = DominoPrivateRoom;
